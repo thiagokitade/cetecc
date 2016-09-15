5316a43120adc891cf737449cc47838b72c7eb62
Thiago Kitade <pavaumk@gmail.com>
2016-09-15 16:35:45 -0300

    Adicionar método para naturalização das rotas angular

--- cetecc/src/main/java/br/com/cetecc/MainController.java
+++ cetecc/src/main/java/br/com/cetecc/MainController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class MainController {
 	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	//Métodos utilizado para naturalizar as rotas. As URLs teram os mesmos nome das rotas do angular
+	@RequestMapping(value = "/{[path:[^\\.]*}")
+	public String redirect() {
+	  return "forward:/";
 	}
 
 }
