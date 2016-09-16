0ff29c99d3a2b79ced39ecbbc9ec6fdc9f030766
Thiago Kitade <pavaumk@gmail.com>
2016-09-16 15:02:23 -0300

    Adicionar nova classe disciplina

---
+++ cetecc/src/main/java/br/com/cetecc/curso/Disciplina.java
@@ -0,0 +1,65 @@
+package br.com.cetecc.curso;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the disciplina database table.
+ * 
+ */
+@Entity
+public class Disciplina implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_disciplina")
+	private long idDisciplina;
+
+	private String descricao;
+
+	private String nome;
+
+	//bi-directional many-to-many association to Curso
+	@OneToMany(mappedBy="curso")
+	private List<Curso> cursos;
+
+	public Disciplina() {
+	}
+
+	public long getIdDisciplina() {
+		return this.idDisciplina;
+	}
+
+	public void setIdDisciplina(long idDisciplina) {
+		this.idDisciplina = idDisciplina;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Curso> getCursos() {
+		return this.cursos;
+	}
+
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+
+}
